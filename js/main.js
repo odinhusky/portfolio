bc4eaf974667bbd10b090871b87d344b2f1831d8
odinhusky <odin19920923@gmail.com>
2019-01-15 17:26:28 +0800

    first update

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function ajaxme(filename) {
+    var dom = $('.p_cont');
+    $.ajax({
+        method: 'post',
+        url: 'ajax/' + filename,
+        dataType: 'html',
+        success: function (call) {
+            console.log('success');
+            $(dom).html(call);
+        },
+        error: function () {
+            console.log('error');
+        },
+        complete: function () {
+            console.log('complete');
+        }
+    })
+}
+
+$(document).ready(function(){
+    ajaxme('ajax1.html');
+    $('.obtn').on('click',function(){
+        var _filename = $(this).attr('data-file');
+        ajaxme(_filename);
+    });
+});
